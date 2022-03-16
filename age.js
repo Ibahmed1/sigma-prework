11cc46cf6c00bf47191ca18ecbaf65d568579082
Ibahmed1 <ib.ahmed1@outlook.com>
2022-03-16 16:48:09 +0000

    created separate function for getting a date from a user and also created an error for if the user enters the dat in the wrong format

--- age.js
+++ age.js
@@ -1,7 +1,18 @@
-function timeSinceADate() {
-  const startDateMilliseconds = Date.parse(
+function getStartDateAndConvertToMilliseconds() {
+  let startDateMilliseconds = Date.parse(
     prompt("Enter a date in the format: yyyy-mm-dd")
   );
+  while (isNaN(startDateMilliseconds)) {
+    startDateMilliseconds = Date.parse(
+      prompt("Wrong format!\nPlease enter a date in the format: yyyy-mm-dd")
+    );
+  }
+  return startDateMilliseconds;
+}
+
+function timeSinceADate() {
+  const startDateMilliseconds = getStartDateAndConvertToMilliseconds();
+
   const currentDateMilliseconds = Date.now();
   const timeSinceStartDate = currentDateMilliseconds - startDateMilliseconds;
   const yearsSinceStartDate = Math.floor(timeSinceStartDate / 31556952000);
